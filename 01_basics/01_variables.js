3cb4510d28908e32b51346028cfaa1c707522947
chelsigoyall <130498516+chelsigoyall@users.noreply.github.com>
2024-10-31 09:29:11 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "chelsi@goggle.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let acountState;
+
+// accountId = 2 now allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "765757"
+accountCity = "goa"
+
+console.log(accountId);
+/*
+Prefer not to use var
+beacuse of issue in block scope & function scope
+*/
+    
+console.table([accountId, accountEmail, accountPassword, accountCity,AccountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert(3 + 3); // We are using Node.js, not the browser
+
+console.log(3
+     +
+      3) // Code readability should be high
+
+console.log("Chelsi");
+
+let name = "Chelsi";
+let age = 18;
+let isLoggedIn = false;
+let state; // This could be initialized if necessary
+
+// Types:
+// number => represents numbers (e.g., 2 to the power of 53)
+// bigint => represents integers with arbitrary precision
+// string => represents text (e.g., "")
+// boolean => represents true or false
+// null => represents a standard value indicating no value
+// undefined => represents a variable that has been declared but not assigned a value
+// symbol => unique identifier
+// object => represents a collection of key-value pairs
+
+console.log(typeof undefined);
